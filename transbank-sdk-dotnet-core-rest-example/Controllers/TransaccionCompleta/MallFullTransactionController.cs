9406f89d974d8a24b1abb38187bd9ae5cea64414
DiegoHBustamante <dhbustamanteaguila@gmail.com>
2024-06-06 10:36:56 -0400

    fix: change variable name token_ws to token in mall full transaction controller

--- transbank-sdk-dotnet-core-rest-example/Controllers/TransaccionCompleta/MallFullTransactionController.cs
+++ transbank-sdk-dotnet-core-rest-example/Controllers/TransaccionCompleta/MallFullTransactionController.cs
@@ -92,7 +92,7 @@ namespace Controllers.TransaccionCompleta
             ViewBag.ChildCommerceCode = child_commerce_code;
             ViewBag.ChildBuyOrder = child_buy_order;
             ViewBag.Amount = 1000;
-            ViewBag.TokenWs = token;
+            ViewBag.Token = token;
 
             ViewBag.StatusEndpoint = CreateUrl(ctrlName, "status");
             ViewBag.RefundEndpoint = CreateUrl(ctrlName, "refund");
@@ -120,7 +120,7 @@ namespace Controllers.TransaccionCompleta
 
             ViewBag.Response = response;
             ViewBag.Amount = 1000;
-            ViewBag.TokenWs = token;
+            ViewBag.Token = token;
             ViewBag.Resp = ToJson(response);
             ViewBag.ChildCommerceCode = child_commerce_code;
             ViewBag.ChildBuyOrder = child_buy_order;
@@ -158,10 +158,10 @@ namespace Controllers.TransaccionCompleta
             return View($"{viewBase}installments.cshtml");
         }
         [Route("status")]
-        public ActionResult status(String token_ws)
+        public ActionResult status(String token)
         {
 
-            var response = tx.Status(token_ws);
+            var response = tx.Status(token);
 
             ViewBag.Response = response;
             var originalResponse = response.OriginalResponse;
@@ -171,14 +171,14 @@ namespace Controllers.TransaccionCompleta
             return View($"{viewBase}status.cshtml");
         }
         [Route("refund")]
-        public ActionResult Refund(String token_ws, int amount, String child_commerce_code, String child_buy_order)
+        public ActionResult Refund(String token, int amount, String child_commerce_code, String child_buy_order)
         {
 
-            var response = tx.Refund(token_ws, child_buy_order, child_commerce_code, amount);
+            var response = tx.Refund(token, child_buy_order, child_commerce_code, amount);
 
             ViewBag.Response = response;
             ViewBag.Resp = ToJson(response);
-            ViewBag.TokenWs = token_ws;
+            ViewBag.Token = token;
 
             return View($"{viewBase}refund.cshtml");
         }
